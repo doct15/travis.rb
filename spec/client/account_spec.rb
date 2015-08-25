6683760f1633455e0dc05109ad1a81c31555b612
Brian McGehee <doc@tavian.com>
2015-08-24 17:33:04 -0700

    Test

--- spec/client/account_spec.rb
+++ spec/client/account_spec.rb
@@ -4,29 +4,29 @@ describe Travis::Client::Account do
   context "from all accounts" do
     let(:session) { Travis::Client.new }
     subject { session.accounts.first }
-    rspec-its(:name) { should be == 'Konstantin Haase' }
-    rspec-its(:login) { should be == 'rkh' }
-    rspec-its(:type) { should be == 'user' }
-    rspec-its(:repos_count) { should be == 200 }
-    rspec-its(:inspect) { should be == "#<Travis::Client::Account: rkh>" }
+    its(:name) { should be == 'Konstantin Haase' }
+    its(:login) { should be == 'rkh' }
+    its(:type) { should be == 'user' }
+    its(:repos_count) { should be == 200 }
+    its(:inspect) { should be == "#<Travis::Client::Account: rkh>" }
   end
 
   context "known account" do
     let(:session) { Travis::Client.new }
     subject { session.account('rkh') }
-    rspec-its(:name) { should be == 'Konstantin Haase' }
-    rspec-its(:login) { should be == 'rkh' }
-    rspec-its(:type) { should be == 'user' }
-    rspec-its(:repos_count) { should be == 200 }
-    rspec-its(:inspect) { should be == "#<Travis::Client::Account: rkh>" }
+    its(:name) { should be == 'Konstantin Haase' }
+    its(:login) { should be == 'rkh' }
+    its(:type) { should be == 'user' }
+    its(:repos_count) { should be == 200 }
+    its(:inspect) { should be == "#<Travis::Client::Account: rkh>" }
   end
 
   context "known account" do
     let(:session) { Travis::Client.new }
     subject { session.account('foo') }
-    rspec-its(:name) { should be_nil }
-    rspec-its(:login) { should be == 'foo' }
-    rspec-its(:type) { should be_nil }
-    rspec-its(:inspect) { should be == "#<Travis::Client::Account: foo>" }
+    its(:name) { should be_nil }
+    its(:login) { should be == 'foo' }
+    its(:type) { should be_nil }
+    its(:inspect) { should be == "#<Travis::Client::Account: foo>" }
   end
 end
